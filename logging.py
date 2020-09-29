bc6255c10d6fa5208554afeecbfa8fbac8a7792e
Mikhail Mishin <36608032+shefferger@users.noreply.github.com>
2020-09-29 13:16:27 +0300

    Add files via upload

---
+++ logging.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from os import path, mkdir
+
+num = 1
+
+
+def getName():
+    global num
+    timeNow = datetime.now()
+    _path = 'logs/' + str(timeNow.day) + '-' + str(timeNow.month) + '-' + str(timeNow.year) + '-' + str(num) + '.txt'
+    if path.exists(_path):
+        num += 1
+        _path = getName()
+    return _path
+
+
+def log(info):
+    print(info)
+    timeNow = datetime.now()
+    with open(fileName, 'a') as logFile:
+        logFile.write('\n[' + str(timeNow) + ']\t' + str(info))
+
+
+if not path.exists('logs'):
+    mkdir('logs', 0o755)
+fileName = getName()
