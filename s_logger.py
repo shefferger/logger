a9cad013d31f53f90a6375df704062f19355f99f
Mikhail Mishin <36608032+shefferger@users.noreply.github.com>
2020-10-31 00:19:45 +0300

    - Some fixes

--- s_logger.py
+++ s_logger.py
@@ -11,22 +11,26 @@ def getName():
     global num, day
     timeNow = datetime.now()
     day = timeNow.day
-    _path = 'logs/log-' + str(timeNow.day) + '-' + str(timeNow.month) + '-' + str(timeNow.year) + '-' + str(num) + '.txt'
+    _path = 'logs/log-' + str(timeNow.year) + '-' + str(timeNow.month) + '-' + str(timeNow.day) + '-' + str(num) + '.txt'
     if path.exists(_path):
         num += 1
         _path = getName()
     return _path
 
 
-def log(info):
+def log(*info):
     timeNow = datetime.now()
+    data = ''
+    for i in info:
+        data += str(i) + ' | '
+    data = data[:-2] + '>>'
     if day != timeNow.day:
         getName()
     if not noPrint:
-        print('\n[' + str(timeNow) + ']\t' + str(info))
+        print('\n[' + str(timeNow) + ']<<  ' + str(data))
     if logIntoFile:
         with open(fileName, 'a') as logFile:
-            logFile.write('\n[' + str(timeNow) + ']\t' + str(info))
+            logFile.write('\n[' + str(timeNow) + ']<<  ' + str(data))
 
 
 if not path.exists('logs'):
