4341c9c03606a6ba0b682b132080f3f1ba2dfdae
Mikhail Mishin <36608032+shefferger@users.noreply.github.com>
2020-10-30 19:08:02 +0300

    - Now starting a new file as day starts

--- s_logger.py
+++ s_logger.py
@@ -2,11 +2,14 @@ from datetime import datetime
 from os import path, mkdir
 
 num = 1
+day = 0
+noPrint = False
 
 
 def getName():
-    global num
+    global num, day
     timeNow = datetime.now()
+    day = timeNow.day
     _path = 'logs/log-' + str(timeNow.day) + '-' + str(timeNow.month) + '-' + str(timeNow.year) + '-' + str(num) + '.txt'
     if path.exists(_path):
         num += 1
@@ -15,8 +18,11 @@ def getName():
 
 
 def log(info):
-    print(info)
     timeNow = datetime.now()
+    if day != timeNow.day:
+        getName()
+    if not noPrint:
+        print('\n[' + str(timeNow) + ']\t' + str(info))
     with open(fileName, 'a') as logFile:
         logFile.write('\n[' + str(timeNow) + ']\t' + str(info))
 
